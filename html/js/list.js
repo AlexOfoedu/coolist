68e89797eb1a7c2a2ce0c3a8ee09a151aff4dc95
alexanderofoedu <alexander.ofoedu@etecture.co.uk>
2015-12-17 17:00:48 +0000

    figuring out eventlistener and function bullshit

--- html/js/list.js
+++ html/js/list.js
@@ -1,4 +1,60 @@
+function bigBomba()
+{
+	
+	xY();
+	changeColor();
+
+};
+
+function xY()
+{
+	
+}
+
 function changeColor()
 {
-document.getElementById("listicles_top").style.color = "#FFFFFF";
+
+	var list = document.getElementById("listicles_top").children;
+
+	var i;
+
+	for (i = 0; i < list.length; i++)
+	{
+		list[i].addEventListener("click", list[i].wZ(this));
+	}
+
 }
+
+function wZ(x)
+{
+	x.style.color = "#000000";
+}
+
+/*
+	function checkItem()
+	{
+
+	var listX = document.getElementById("listicles_top").children;
+	var i;
+
+		for (i = 0; i < listX.length; i++)
+		{
+			listX[i].addEventListener("click", alert(listX[i]));
+		};
+	};
+
+	function addItem()
+	{
+		document.getElementById("btn_add").addEventListener("click", alert("YO!"));
+	};
+
+};
+*/
+/*
+function itemColorChange(theItem)
+{
+
+	theItem.style.color = "#000000";
+
+};
+*/
